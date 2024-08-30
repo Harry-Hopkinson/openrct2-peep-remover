cad28e60be4364e6e3cd5efc352b81bbf68f316d
Harry Hopkinson <harryjhopkinson@gmail.com>
2024-08-30 22:12:48 +0100

    Show information notice if trying to remove peeps with none selected.

--- src/utils/RemovePeeps.ts
+++ src/utils/RemovePeeps.ts
@@ -3,6 +3,9 @@ import { PeepIds } from "./stores";
 export function RemovePeep(): void {
   const SelectedPeepIds = PeepIds.get();
 
+  if (SelectedPeepIds.length == 0)
+    return ui.showError("Select Peeps", "You must select a peep to remove");
+
   SelectedPeepIds.forEach((id) => {
     const entity = map.getEntity(id);
     if (entity && (entity.type === "guest" || entity.type === "staff")) {
