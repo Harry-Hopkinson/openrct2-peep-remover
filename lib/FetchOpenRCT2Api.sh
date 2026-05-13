fe6e9696645eba0264f1d2bdbe4069beb7995d7d
Harry Hopkinson <harryjhopkinson@gmail.com>
2026-05-13 14:42:27 +0100

    Upgrade packages and openrct2.d.ts link

--- lib/FetchOpenRCT2Api.sh
+++ lib/FetchOpenRCT2Api.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/OpenRCT2/OpenRCT2/develop/distribution/openrct2.d.ts
+wget https://raw.githubusercontent.com/OpenRCT2/OpenRCT2/develop/distribution/scripting/openrct2.d.ts
